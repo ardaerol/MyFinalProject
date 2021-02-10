33fef3cbf9ba47f6eb31eb521ba253e7bace2d09
Aziz Arda Erol <20190311036@dogus.edu.tr>
2021-02-11 00:03:31 +0300

    10 subat son

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -9,15 +9,15 @@ namespace Business.Abstract
 {
    public interface IProductService
     {
-        List<Product> GetAllProduct();
+        IDataResult<List<Product>> GetAllProduct();
 
-        List<Product> GetAllByCategoryId(int id);
+       IDataResult<List<Product>> GetAllByCategoryId(int id);
 
-        List<Product> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
 
-        List<ProductDetailDto> GetProductDetails();
+       IDataResult<List<ProductDetailDto>> GetProductDetails();
 
-        Product GetById(int productId);
+        IDataResult<Product> GetById(int productId);
 
         IResult Add(Product product);
     }
